7f8636b08be75c08568cc60da16d4eda5bbb2f78
Tate-Smith <tatelansing@gmail.com>
2026-01-08 17:49:30 -0800

    Inital commit

---
+++ src/main/java/com/tatesmith/caching_proxy/caching_proxy/App.java
@@ -0,0 +1,10 @@
+package com.tatesmith.caching_proxy.caching_proxy;
+
+/**
+ * Hello world!
+ */
+public class App {
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
